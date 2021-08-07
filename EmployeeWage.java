4ced56dfc05e238f065c7e23c77b9f4589a516d5
moshtab <mdmohsin41439@gmail.com>
2021-08-07 20:46:11 +0530

    Calculating wage for 20 days

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -8,26 +8,26 @@ public class EmployeeWage {
 		int workingHr = 0;
 		int empWage = 0;
 		int wagePerHr = 20;
-		Random r = new Random();
-		int empCheck = r.nextInt(3);
-		switch (empCheck) {
-		case 0: {
-			System.out.println("Employee is present");
-			workingHr = 8;
-			break;
+		for (int i = 0; i < 20; i++) {
+			Random r = new Random();
+			int empCheck = r.nextInt(3);
+			switch (empCheck) {
+			case 0: {
+
+				workingHr = 8;
+				break;
+			}
+			case 1: {
+				workingHr = 4;
+				break;
+			}
+			case 2: {
+				workingHr = 0;
+				break;
+			}
+			}
+			empWage = empWage + (workingHr * wagePerHr);
 		}
-		case 1: {
-			System.out.println("Employee is partTime");
-			workingHr = 4;
-			break;
-		}
-		case 2: {
-			System.out.println("Employee is absent");
-			workingHr = 0;
-			break;
-		}
-		}
-		empWage = empWage + (workingHr * wagePerHr);
 		System.out.println("Employee wage is : " + empWage);
 
 	}
