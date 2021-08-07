d8cf9e33210a3d9d8abbc6d414b894415ba46952
moshtab <mdmohsin41439@gmail.com>
2021-08-07 20:56:11 +0530

    Added condition of maxHrs=100  and No. of workingdays=20

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -5,10 +5,15 @@ import java.util.Random;
 public class EmployeeWage {
 
 	public static void calculatingWage() {
+		int empHrs = 0;
+		int totalWorkingDays = 0;
 		int workingHr = 0;
 		int empWage = 0;
 		int wagePerHr = 20;
-		for (int i = 0; i < 20; i++) {
+		int maxHrs = 100;
+		int noOfWorkingDays = 100;
+
+		while (empHrs <= maxHrs && totalWorkingDays <= noOfWorkingDays) {
 			Random r = new Random();
 			int empCheck = r.nextInt(3);
 			switch (empCheck) {
@@ -26,6 +31,7 @@ public class EmployeeWage {
 				break;
 			}
 			}
+			empHrs += workingHr;
 			empWage = empWage + (workingHr * wagePerHr);
 		}
 		System.out.println("Employee wage is : " + empWage);
