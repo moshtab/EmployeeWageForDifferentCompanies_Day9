7f9d4bfa16fcca07c6d68c95e2b9114f515cde4a
moshtab <mdmohsin41439@gmail.com>
2021-08-07 20:11:41 +0530

    Display message

---
+++ EmployeeWage.java
@@ -0,0 +1,10 @@
+package EmployeeWageForDifferentCompanies;
+
+public class EmployeeWage {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee wage Computation Program");
+
+	}
+
+}
